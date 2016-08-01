512f4954cbd6daf94db62b90a477a27cd61b7579
vitaly <goodgame1945@gmail.com>
2016-08-01 12:52:49 +0300

    investing ready

--- scrapy.py
+++ scrapy.py
@@ -1,4 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
+HEADERS = {'User-agent': 'Mozilla/5.0'}
+currencies = open('currencies.txt').readlines()
+for currency in currencies:
+    url = 'http://www.investing.com/' + currency.rstrip()
+    r = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(r.content, "lxml")
+    span = soup.find(id="last_last")
+    print(span.contents[0])
 
+# '<span class="arial_26 (.+?)" id="last_last" dir="ltr">(.+?)</span>'
