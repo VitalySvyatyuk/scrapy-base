2e056a6ef7ded7269ab5d6dcc4fc28f9a45be979
Vitalysvyatyuk <goodgame1945@gmail.com>
2016-08-12 19:59:22 +0300

    Without React

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from scrapy import current_data
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route("/")
-def hello():
-    return "Hi"
+def index(data=current_data):
+    return render_template("index.html", data=data)
 
 if __name__ == "__main__":
     app.run()
--- scrapy.py
+++ scrapy.py
@@ -4,9 +4,11 @@ import re
 
 HEADERS = {'User-agent': 'Mozilla/5.0'}
 currencies = open('currencies.txt').readlines()
+current_data = {}
 for currency in currencies:
     url = 'http://www.investing.com/' + currency.rstrip()
     r = requests.get(url, headers=HEADERS)
     soup = BeautifulSoup(r.content, "lxml")
     span = soup.find(id="last_last")
-    print(span.contents[0])
+    current_data[currency.rstrip().split("/")[1]] = (span.contents[0])
+print(current_data)
