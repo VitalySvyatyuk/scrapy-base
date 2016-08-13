e7128f8186147d57e971094c9abc7176e1c0efae
Vitalysvyatyuk <goodgame1945@gmail.com>
2016-08-13 13:34:55 +0300

    while endless loop

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 from flask import Flask, render_template
 from scrapy import current_data
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'currencies.db')
+db = SQLAlchemy(app)
+
+
+class Currency(db.Model):
+    __tablename__ = 'currencies'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    usd_rub = db.Column(db.String(8))
+    eur_rub = db.Column(db.String(8))
+    brent_oil = db.Column(db.String(8))
+    
+    def __init__(self, date, usd_rub, eur_rub, brent_oil):
+        self.date = date
+        self.usd_rub = usd_rub
+        self.eur_rub = eur_rub
+        self.brent_oil = brent_oil
+
 
 @app.route("/")
 def index(data=current_data):
+    curr = Currency(str(datetime.now()), data['usd-rub'],
+                    data['eur-rub'], data['brent-oil'])
+    db.session.add(curr)
+    db.session.commit()
     return render_template("index.html", data=data)
 
 if __name__ == "__main__":
     app.run()
+
--- scrapy.py
+++ scrapy.py
@@ -1,14 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
 
 HEADERS = {'User-agent': 'Mozilla/5.0'}
-currencies = open('currencies.txt').readlines()
-current_data = {}
-for currency in currencies:
-    url = 'http://www.investing.com/' + currency.rstrip()
-    r = requests.get(url, headers=HEADERS)
-    soup = BeautifulSoup(r.content, "lxml")
-    span = soup.find(id="last_last")
-    current_data[currency.rstrip().split("/")[1]] = (span.contents[0])
-print(current_data)
+def scrap():
+    currencies = open('currencies.txt').readlines()
+    current_data = {}
+    for currency in currencies:
+        url = 'http://www.investing.com/' + currency.rstrip()
+        r = requests.get(url, headers=HEADERS)
+        soup = BeautifulSoup(r.content, "lxml")
+        span = soup.find(id="last_last")
+        current_data[currency.rstrip().split("/")[1]] = (span.contents[0])
+    print(current_data)
+
+n = 0
+while n == 0:
+    time.sleep(5)
+    scrap()
+    print("Ok")
